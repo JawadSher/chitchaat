6fe7f09e5aa7f5096b498af0e926f52c5c55f4dd
JawadSher <jawadsher04@gmail.com>
2026-02-04 21:47:19 +0500

    feat: fix some more suspense related issues in home app/layout

--- src/app/(home)/app/layout.tsx
+++ src/app/(home)/app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import MainHeader from "@/components/main-header";
-import React from "react";
+import React, { Suspense } from "react";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { MessageSquareText, Phone } from "lucide-react";
 import { useSearchParams, useRouter } from "next/navigation";
@@ -26,7 +26,7 @@ const icons = {
   Phone,
 };
 
-function AppLayout({ children }: { children: React.ReactNode }) {
+function AppLayoutContent({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -43,6 +43,58 @@ function AppLayout({ children }: { children: React.ReactNode }) {
     router.replace(`?${params.toString()}`);
   };
 
+  return (
+    <Tabs
+      value={currentTab}
+      onValueChange={handleTabChange}
+      className="flex flex-1 overflow-hidden"
+    >
+      <div className="flex flex-1 overflow-hidden">
+        <div className="max-w-fit bg-sidebar flex flex-col justify-between">
+          <TabsList className="flex bg-sidebar flex-col flex-1 items-center justify-start gap-2 p-2 border-none rounded-none w-14">
+            {items.map((item) => {
+              const Icon = icons[item.Icon];
+              return (
+                <TabsTrigger
+                  key={item.value}
+                  value={item.value}
+                  className="flex items-center justify-center gap-2 rounded-full border-none max-h-9 min-w-9 p-2 cursor-pointer"
+                >
+                  <Icon className="size-5" strokeWidth={1.89} />
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          <div className="w-full h-fit flex flex-col items-center gap-2 p-2">
+            <ModeToggle />
+            <SignedOut>
+              <SignInButton>
+                <Button className="bg-primary rounded-full font-medium text-sm sm:text-base px-4 sm:px-5 cursor-pointer">
+                  Sign in
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton
+                appearance={{
+                  elements: {
+                    avatarBox: "min-w-9 min-h-9 border-2 border-red",
+                  },
+                }}
+              />
+            </SignedIn>
+          </div>
+        </div>
+
+        <div className="flex-1 h-full bg-background border-primary border-r-0 border border-l border-t rounded-tl-lg overflow-clip">
+          {children}
+        </div>
+      </div>
+    </Tabs>
+  );
+}
+
+function AppLayout({ children }: { children: React.ReactNode }) {
   const { isLoaded } = useUser();
 
   if (!isLoaded) {
@@ -52,54 +104,9 @@ function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-screen flex flex-col">
       <MainHeader />
-
-      <Tabs
-        value={currentTab}
-        onValueChange={handleTabChange}
-        className="flex flex-1 overflow-hidden"
-      >
-        <div className="flex flex-1 overflow-hidden">
-          <div className="max-w-fit bg-sidebar flex flex-col justify-between">
-            <TabsList className="flex bg-sidebar flex-col flex-1 items-center justify-start gap-2 p-2 border-none rounded-none w-14">
-              {items.map((item) => {
-                const Icon = icons[item.Icon];
-                return (
-                  <TabsTrigger
-                    key={item.value}
-                    value={item.value}
-                    className="flex items-center justify-center gap-2 rounded-full border-none max-h-9 min-w-9 p-2 cursor-pointer"
-                  >
-                    <Icon className="size-5" strokeWidth={1.89} />
-                  </TabsTrigger>
-                );
-              })}
-            </TabsList>
-            <div className="w-full h-fit flex flex-col items-center gap-2 p-2">
-              <ModeToggle />
-              <SignedOut>
-                <SignInButton>
-                  <Button className="bg-primary rounded-full font-medium text-sm sm:text-base px-4 sm:px-5 cursor-pointer">
-                    Sign in
-                  </Button>
-                </SignInButton>
-              </SignedOut>
-              <SignedIn>
-                <UserButton
-                  appearance={{
-                    elements: {
-                      avatarBox: "min-w-9 min-h-9 border-2 border-red",
-                    },
-                  }}
-                />
-              </SignedIn>
-            </div>
-          </div>
-
-          <div className="flex-1 h-full bg-background border-primary border-r-0 border border-l border-t rounded-tl-lg overflow-clip">
-            {children}
-          </div>
-        </div>
-      </Tabs>
+      <Suspense fallback={<SplashScreen />}>
+        <AppLayoutContent>{children}</AppLayoutContent>
+      </Suspense>
     </div>
   );
 }

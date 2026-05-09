a901428988869cc24c27d883b946347ec9149669
JawadSher <jawadsher04@gmail.com>
2026-05-09 15:46:19 +0500

    fix(webhook): correct receiver initialization and improve error handling in POST request

--- src/app/api/webhooks/livekit/route.ts
+++ src/app/api/webhooks/livekit/route.ts
@@ -1,24 +1,41 @@
 import { WebhookReceiver } from "livekit-server-sdk";
 import { NextRequest, NextResponse } from "next/server";
 
-const reciever = new WebhookReceiver(
+const receiver = new WebhookReceiver(
   process.env.LIVEKIT_API_KEY!,
   process.env.LIVEKIT_API_SECRET!,
 );
+
 export async function POST(request: NextRequest) {
   try {
-    const body = await request.json();
-    const authorization = body.get("Authorization");
-    const event = reciever.receive(body, authorization);
+    const body = await request.text();
+
+    const authorization = request.headers.get("Authorization");
+
+    if (!authorization) {
+      return NextResponse.json({
+        success: false,
+        error: "Missing Authorization header",
+      });
+    }
+
+    const event = await receiver.receive(body, authorization);
 
     console.log(event);
 
-    return NextResponse.json({ status: 200, success: true });
-  } catch (error: any) {
     return NextResponse.json({
-      error: error.message,
-      status: 500,
-      success: false,
+      success: true,
+      status: 200,
     });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: error.message,
+      },
+      {
+        status: 500,
+      },
+    );
   }
 }

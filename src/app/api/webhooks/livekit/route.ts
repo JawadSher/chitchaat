2ab0c34fcde68d89cdd8cc0d28a15466ea6de6d3
JawadSher <jawadsher04@gmail.com>
2026-05-09 15:39:34 +0500

    feat(webhook): integrate WebhookReceiver for event handling in POST request

--- src/app/api/webhooks/livekit/route.ts
+++ src/app/api/webhooks/livekit/route.ts
@@ -1,13 +1,17 @@
+import { WebhookReceiver } from "livekit-server-sdk";
 import { NextRequest, NextResponse } from "next/server";
 
+const reciever = new WebhookReceiver(
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!,
+);
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
-
-    const event = body.event;
+    const authorization = body.get("Authorization");
+    const event = reciever.receive(body, authorization);
 
     console.log(event);
-    console.log(body.room);
 
     return NextResponse.json({ status: 200, success: true });
   } catch (error: any) {
